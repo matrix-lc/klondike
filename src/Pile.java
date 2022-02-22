6307b08ef06df0558854248c3d7b1a8b454ee96f
Sanya773 <70611849+Sanya773@users.noreply.github.com>
2022-02-22 09:26:48 -0800

    Update Pile.java

--- src/Pile.java
+++ src/Pile.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.awt.*;
 
 /** A Pile is a collection of cards.  This needs to be
  * Drawable because it will be shown on the GUI. Put code
@@ -7,6 +9,41 @@
  * You WILL write subclasses of Pile
  */
 public abstract class Pile implements Drawable, Updateable {
-    
+    private ArrayList<Card> cards = new ArrayList<>();
+
+    public abstract double getxLoc();
+    public abstract double getyLoc();
+
     public abstract boolean canAddCard(Card c);
+    
+    public void draw(Graphics g) {
+        if (cards.size() == 0) {
+            return;
+        }
+        for (Card x : cards) {
+            x.draw(g);
+        }
+    }
+
+    public void addCard(Card c){
+        cards.add(c);
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+
+    public ArrayList<Card> getList() {
+        return cards;
+    }
+
+    public Card getTopCard() {
+        if (cards.size() > 0) {
+            return cards.get(1);
+        }
+        return null;
+    }
+
+    //public void removeCard(); --> better to leave for subclasses
+    // for suitpiles, draw only last card bc loc is same as piles loc
 }
