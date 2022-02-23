abaae1227dbf1e14231d573b2053f932adcb6651
Sanya773 <70611849+Sanya773@users.noreply.github.com>
2022-02-23 09:12:22 -0800

    Create DrawPile.java

---
+++ src/DrawPile.java
@@ -0,0 +1,36 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+
+public class DrawPile extends Pile{
+
+    @Override
+    public void update(ActionEvent a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean canAddCard(Card c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public double getxLoc() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getyLoc() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
