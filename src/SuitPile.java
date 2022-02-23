3af30022ece375f9b4a617c5fe12564cf03f92b3
Sanya773 <70611849+Sanya773@users.noreply.github.com>
2022-02-23 09:10:00 -0800

    Create SuitPile.java

---
+++ src/SuitPile.java
@@ -0,0 +1,36 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+
+public class SuitPile extends Pile{
+
+    @Override
+    public boolean canAddCard(Card c) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public double getxLoc() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getyLoc() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void update(ActionEvent a) {
+        // TODO Auto-generated method stub
+        // ignore   
+    }
+    
+}
