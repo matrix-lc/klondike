6bef72cca3902d404968a0858a6fe5c54b89d104
matrix-lc <58668395+matrix-lc@users.noreply.github.com>
2022-02-23 09:06:54 -0800

    aasdasd

---
+++ src/drawPile.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+import java.awt.event.*;
+
+import javax.imageio.ImageIO;
+
+public class drawPile extends Pile {
+    ArrayList<Card> drawPile = ArrayList<>();
+    
+    public void double getxLoc(){
+
+    }
+
+    public void double getyLoc(){
+
+    }
+
+    public void boolean canAddCard(Card c){
+
+    }
+}
