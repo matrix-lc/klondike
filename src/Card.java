87040873ac55b161a593fb337fcf9857c39a2be7
nishantdoss <97282431+nishantdoss@users.noreply.github.com>
2022-02-25 08:48:12 -0800

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,6 +1,10 @@
 import java.awt.Graphics;
-import java.awt.event.*;
-
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 import javax.imageio.ImageIO;
 
 /** represents a playing card that can draw itself. */
@@ -8,36 +12,62 @@ public class Card implements Drawable, Updateable{
     //Card getLocationX(), getLocationY();
 		//move(Pile p) --> moves a Card to a Pile;
 		//faceUp(); --> returns whether the card is face up or face down;
-
-    private int xLoc, yLoc;
-    private boolean faceUp;
+    private int value, suit;
+    private boolean faceUp, selected;
     private Image frontImage;
-    static private Image backImage;
+    private Location loc;
+    private static Image backImage;
+    private static ArrayList<String> cardSuit = new ArrayList<String>(List.of("", "Hearts", "Diamonds", "Clubs", "Spades"));
+    private static ArrayList<String> cardValue = new ArrayList<>(List.of("1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"));
 
 
+    public Card(int s, int v){
+        this.suit = s;
+        this.value = v;
+        this.loc = new Location (0,0);
+        openImage();
+    }
 
-    public Card(int x, int y, boolean up){
-        this.xLoc = x;
-        this.yLoc = y;
-        this.faceUp = up;
+    public Card(int s, int v, int x, int y){
+        this.suit = s;
+        this.value = v;
+        this.loc = new Location (x,y);
+        openImage();
+    }
+
+    public void openImage(){
+        String s = "Klondike/images/cards/";
+        if(!faceUp){
+            s+="b1fv.png";
+        }
+        else{
+            s += cardSuit.get(suit) + cardValue.get(value) + ".png";
+        }
+    }
+		
+
+
+    public int compareTo(Card c){
+        return this.value - c.value;
     }
 
 
-    public int getLocationX(){
-        return xLoc;
-    }
-
-    public int getLocationY(){
-        return yLoc;
-    }
 
 
-
-    public void draw(Graphics g){
+    public void moveTo(Loacation newLoc){
+        loc = newLoc;
 
     }
 
-    public void update(ActionEvent e){
-
+    @Override
+    public String toString(){
+        String s = "";
+        if(value < 11) {
+            s += value;
+        }
+        else{
+            s+= cardSuit.gets(value - 6);
+        }
+        return s + "of" + cardSuit.get(suit);
     }
 }
