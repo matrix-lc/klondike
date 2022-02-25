044d5b7d01f58e295630c7ba88e4de12bf54dcbc
nishantdoss <97282431+nishantdoss@users.noreply.github.com>
2022-02-25 08:59:36 -0800

    Create Location.java

---
+++ src/Location.java
@@ -0,0 +1,16 @@
+public class Location {
+    private double x,y;
+
+    public Location(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getXLoc(){
+        return x;
+    }
+
+    public double getYLoc(){
+        return y;
+    }
+}
