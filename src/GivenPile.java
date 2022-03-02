2b351d8f584fefd6385430014f2968785dc551a1
matrix-lc <58668395+matrix-lc@users.noreply.github.com>
2022-03-02 09:48:21 -0800

    Update GivenPile.java

--- src/GivenPile.java
+++ src/GivenPile.java
@@ -1,7 +1,15 @@
+import java.awt.*;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class GivenPile extends Pile{
+
+    private Image testImage, backImage;
 
     @Override
     public void update(ActionEvent a) {
@@ -19,6 +27,20 @@ public class GivenPile extends Pile{
     public void draw(Graphics g) {
         // TODO Auto-generated method stub
         
+        try {
+			testImage = ImageIO.read(new File("images/cards/dj.png"));
+			backImage = ImageIO.read(new File("images/cards/b1fv.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+        // g.drawImage(testImage, 30, 80, null);
+		// g.drawImage(backImage, 100, 80, null);
+		// g.drawImage(backImage, 105, 100, null);
+
+        for (Card x: cards){
+            g.drawImage(backImage, x.getLoc().getXLoc(), x.getLoc().getYLoc(), null);
+        }
     }
 
     @Override
